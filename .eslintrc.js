a0f73a5fba75ad9be214c2cd802718e4430800e6
Pixelastic <tim@pixelastic.com>
2020-05-26 14:58:22 +0200

    feat(norska): Add norska

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,3 @@
 module.exports = {
-  extends: ['./node_modules/aberlaas/eslint.js'],
+  extends: ['./node_modules/aberlaas/lib/configs/eslint.js'],
 };
---
+++ scripts/downloadImages.js
@@ -0,0 +1,25 @@
+const readJson = require('firost/lib/readJson');
+const download = require('firost/lib/download');
+const spinner = require('firost/lib/spinner');
+const _ = require('golgoth/lib/lodash');
+const pMap = require('golgoth/lib/pMap');
+const path = require('path');
+
+(async function() {
+  const items = await readJson('records/baldur.json');
+  const progress = spinner(items.length);
+  await pMap(
+    items,
+    async item => {
+      const name = _.camelCase(item.title);
+      const pictureUrl = item.picture;
+      const downloadPath = path.resolve(
+        `src/assets/pictures/baldur/${name}.png`
+      );
+      progress.tick(name);
+      await download(pictureUrl, downloadPath);
+    },
+    { concurrency: 5 }
+  );
+  progress.success('ok');
+})();
