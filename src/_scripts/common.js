5193526e579bf6a77352530c21bdb855307af2c5
Pixelastic <tim@pixelastic.com>
2020-07-03 17:04:52 +0200

    fix(styling): Display other items of the same type

---
+++ src/_scripts/common.js
@@ -0,0 +1,87 @@
+const lazyload = require('norska/frontend/lazyload');
+const algolia = require('norska/frontend/algolia');
+const {
+  configure,
+  hits,
+  pagination,
+  refinementList,
+  searchBox,
+} = require('norska/frontend/algolia/widgets');
+const credentials = window.CONFIG.algolia;
+const transforms = require('./transforms.js');
+
+module.exports = {
+  run() {
+    const widgets = [
+      /**
+       * Main configuration
+       **/
+      {
+        type: configure,
+        options: {
+          hitsPerPage: 100,
+        },
+      },
+      /**
+       * Searchbar
+       **/
+      {
+        type: searchBox,
+        options: {
+          container: '#searchbox',
+          placeholder: 'Search for item name or description',
+          autofocus: true,
+          showReset: false,
+          showSubmit: false,
+          showLoadingIndicator: false,
+        },
+      },
+      {
+        type: refinementList,
+        options: {
+          container: '#type',
+          attribute: 'type',
+          limit: 8,
+          sortBy: ['count:desc', 'name:asc'],
+        },
+      },
+      /**
+       * Hits
+       **/
+      {
+        type: hits,
+        options: {
+          container: '#hits',
+          templates: {
+            item: document.getElementById('hitTemplate').value,
+            empty: document.getElementById('emptyTemplate').value,
+          },
+        },
+      },
+      {
+        type: pagination,
+        options: {
+          container: '#pagination',
+        },
+      },
+    ];
+
+    algolia
+      .init(credentials)
+      .setWidgets(widgets)
+      .setTransforms(transforms)
+      .onSearch((query) => {
+        if (!query) {
+          document.body.removeAttribute('data-hasQuery');
+          return;
+        }
+        document.body.setAttribute('data-hasQuery', '');
+      })
+      // .onDisplay(hit => {
+      //   console.info(hit.picture);
+      // })
+      .start();
+
+    lazyload.init();
+  },
+};
---
+++ src/_scripts/item.js
@@ -0,0 +1,34 @@
+const algolia = require('norska/frontend/algolia');
+const { configure, hits } = require('norska/frontend/algolia/widgets');
+const credentials = window.CONFIG.algolia;
+const item = window.ITEM;
+const transforms = require('./transforms.js');
+
+module.exports = {
+  run() {
+    const widgets = [
+      {
+        type: configure,
+        options: {
+          hitsPerPage: 4,
+          facetFilters: [`type:${item.type}`],
+        },
+      },
+      {
+        type: hits,
+        options: {
+          container: '#previews',
+          templates: {
+            item: document.getElementById('previewTemplate').value,
+          },
+        },
+      },
+    ];
+
+    algolia
+      .init(credentials)
+      .setWidgets(widgets)
+      .setTransforms(transforms)
+      .start();
+  },
+};
